0dc3aa1112383e773935fbfe4be4350bfb589e6f
Caaabooomm <85229948+gabriel-vitor-nave@users.noreply.github.com>
2026-09-17 22:24:22 -0300

    trabalhando

---
+++ critical!/Assets/ImportedAssets/PixelizeIt/PixelizePass.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+
+public class PixelizePass : ScriptableRenderPass
+{
+    private PixelizeFeature.CustomPassSettings settings;
+}
