d5dc2abe2a5e432630b8f0b28494d54848ebcbbc
Isaac-Andrade-Santos-NAVE <85229948+gabriel-vitor-nave@users.noreply.github.com>
2026-09-21 08:43:31 -0300

    discard all changes

--- critical/Assets/_Scripts/Player/PlayerMovement.cs
+++ critical/Assets/_Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem; // Obrigatório para o CallbackContext
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
     public float speedMultiplier = 1f;
 
     private Rigidbody2D rb2D;
-    private Animator animator;
+    private Vector2 inputDirecao; 
 
-    void Awake()
+    public void Awake()
     {
         rb2D = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        inputDirecao = context.ReadValue<Vector2>().normalized;
+    }
+
+    public void FixedUpdate()
+    {
+        rb2D.velocity = inputDirecao * (speed * speedMultiplier);
     }
 }
